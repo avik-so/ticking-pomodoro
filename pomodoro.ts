5e520577787079396f4c3c9a8d3a4ae98977b7ad
Avi Kessner <avik@scheduleonce.com>
2019-12-15 11:34:29 +0200

    Creating Tomotato, and setting times.

--- pomodoro.ts
+++ pomodoro.ts
@@ -10,15 +10,20 @@ export class Pomodoro   {
 
        } else {
         let config = time.split(" ");
-     
-       if (config[1] == "min") {
-           ms =  parseInt(config[0]) * 60 * 1000;
-       } else if (config[1] == "sec") {
-           ms = parseInt(config[0]) * 1000;
-       }
+        ms = config.length == 1 ? parseInt(time) * 1000 : this.parseTimeInterval(config)
+       
     }
        this.shortMs = ms;
        return ms;
+    }
+    parseTimeInterval(config: string[]): number {
+        let ms:number = 0;
+        if (config[1] == "min") {
+            ms =  parseInt(config[0]) * 60 * 1000;
+        } else if (config[1] == "sec") {
+            ms = parseInt(config[0]) * 1000;
+        }
+        return ms;
     }
 
 }
